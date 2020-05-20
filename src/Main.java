da2b9a173f22bb3457bd6082f01b246350357971
bouboul122 <boul0902@usherbrooke.ca>
2020-05-19 20:54:24 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main
     public static void main(String[] args)
     {
         FeeCalculator feeCalc = new FeeCalculator(30, 14);
-        
+
         System.out.println("Hello world");
     }
 }
