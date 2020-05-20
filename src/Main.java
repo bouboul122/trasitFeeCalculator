716c2a5f454968626282847f7c77399449640c8d
bouboul122 <boul0902@usherbrooke.ca>
2020-05-19 20:42:35 -0400

    Tested object in main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
+        FeeCalculator feeCalc = new FeeCalculator(30, 14);
+        
         System.out.println("Hello world");
-
     }
 }
